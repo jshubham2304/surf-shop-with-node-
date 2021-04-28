af1bcd263e84b9357b2c77699fcc2e19062dc2e4
Shubham Jain <jshubham2304@gmail.com>
2021-04-28 19:01:07 +0530

    feat:CRUD on Image with cloundiary

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,39 +1,84 @@
 const Post = require('../models/post');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+	cloud_name: 'dui7rjl7p',
+	api_key: '997977887133522',
+	api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports = {
-	async getPosts(req, res, next) {
+	// Posts Index
+	async postIndex(req, res, next) {
 		let posts = await Post.find({});
 		res.render('posts/index', { posts });
 	},
-	newPosts(req,res,next){
-		res.render(
-			'posts/new'
-		)
+	// Posts New
+	postNew(req, res, next) {
+		res.render('posts/new');
 	},
-	async createPosts(req,res,next){
-		let post = await Post.create(req.body);
-		res.redirect(`/posts/${post.id}`)
+	// Posts Create
+	async postCreate(req, res, next) {
+		req.body.post.images = [];
+		for(const file of req.files) {
+			let image = await cloudinary.v2.uploader.upload(file.path);
+			req.body.post.images.push({
+				url: image.secure_url,
+				public_id: image.public_id
+			});
+		}
+		let post = await Post.create(req.body.post);
+		res.redirect(`/posts/${post.id}`);
 	},
-	async showPosts(req, res, next) {
-		let posts = await Post.findById(req.params.id);
-		console.log(posts);
-		res.render('posts/show', { posts });
+	// Posts Show
+	async postShow(req, res, next) {
+		let post = await Post.findById(req.params.id);
+		res.render('posts/show', { post });
 	},
-	async editPosts(req, res, next) {
-		let posts = await Post.findById(req.params.id);
-		
-		res.render('posts/edit', { posts });
+	// Posts Edit
+	async postEdit(req, res, next) {
+		let post = await Post.findById(req.params.id);
+		res.render('posts/edit', { post });
 	},
+	// Posts Update
 	async postUpdate(req, res, next) {
-		
-		let posts = await Post.findByIdAndUpdate(req.params.id, req.body.posts,{new:true});
-		
-		res.redirect(`/posts/${posts.id}`)
+		let post = await Post.findById(req.params.id);
+		if(req.body.deleteImages && req.body.deleteImages.length ){
+			let deleteImg  = req.body.deleteImages;
+			for(const public_id of deleteImg){
+				await cloudinary.v2.uploader.destroy(public_id);
+				for(const image of post.images){
+					if(image.public_id ===public_id){
+						let index=  post.images.indexOf(image);
+						post.images.splice(index,1);
+					}
+				}
+			}
+			if(req.files){
+				for(const file of req.files) {
+					let image = await cloudinary.v2.uploader.upload(file.path);
+					post.images.push({
+						url: image.secure_url,
+						public_id: image.public_id
+					});
+				}
+					
+			}
+		}
+		// handle existing images
+		post.description = req.body.post.description;
+		post.price = req.body.post.price;
+		post.location = req.body.post.location;
+		//handle upload 
+		post.save();
+		res.redirect(`/posts/${post.id}`);
 	},
-	async deletePost(req, res, next) {
-		
-		let posts = await Post.findByIdAndDelete(req.params.id);
-		
-		res.redirect(`/posts`)
-	},
+	// Posts Destroy
+	async postDestroy(req, res, next) {
+		let post =await Post.findById(req.params.id);
+		for(const image of post.images){
+			await cloudinary.v2.uploader.destroy(image.public_id);		
+		}
+		await post.remove();
+		res.redirect('/posts');
+	}
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,34 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const { errorHandler } = require('../middleware');
-const { getPosts , 
-   newPosts, 
-   createPosts,
-   showPosts,
-   editPosts,
-   postUpdate, deletePost} = require('../controllers/posts');
+const multer = require('multer');
+const upload = multer({'dest': 'uploads/'});
+const { asyncErrorHandler } = require('../middleware');
+const { 
+	postIndex,
+	postNew,
+  postCreate,
+  postShow,
+  postEdit,
+  postUpdate,
+  postDestroy
+} = require('../controllers/posts');
 
 /* GET posts index /posts */
-router.get('/', errorHandler(getPosts));
+router.get('/', asyncErrorHandler(postIndex));
 
 /* GET posts new /posts/new */
-router.get('/new', errorHandler(newPosts));
+router.get('/new', postNew);
 
 /* POST posts create /posts */
-router.post('/',errorHandler( createPosts));
+router.post('/', upload.array('images', 4), asyncErrorHandler(postCreate));
 
 /* GET posts show /posts/:id */
-router.get('/:id', errorHandler(showPosts));
+router.get('/:id', asyncErrorHandler(postShow));
 
 /* GET posts edit /posts/:id/edit */
-router.get('/:id/edit',errorHandler( editPosts));
+router.get('/:id/edit', asyncErrorHandler(postEdit));
 
 /* PUT posts update /posts/:id */
-router.put('/:id', errorHandler(postUpdate));
+router.put('/:id',upload.array('images',4), asyncErrorHandler(postUpdate));
 
 /* DELETE posts destroy /posts/:id */
-
-router.delete('/:id', errorHandler(deletePost));
+router.delete('/:id', asyncErrorHandler(postDestroy));
 
 
 module.exports = router;
