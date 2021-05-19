42dcfac0843d11c4a60517ab7df76fcd8ffce20d
Shubham Jain <jshubham2304@gmail.com>
2021-05-19 22:55:10 +0530

    fix: Review

--- controllers/index.js
+++ controllers/index.js
@@ -4,14 +4,19 @@ const passport = require('passport');
 module.exports = {
 	// POST /register
 	async postRegister(req, res, next) {
+		try{
 		const newUser = new User({
 			username: req.body.username,
 			email: req.body.email,
 			image: req.body.image
 		});
+		console.log(newUser);
 
 		await User.register(newUser, req.body.password);
 		res.redirect('/');
+	}catch(e){
+			res.status(400).send({error:e.message});
+		}
 	},
 	// POST /login
 	postLogin(req, res, next) {
--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+	username:String,
 	email: String,
 	image: String
 });
